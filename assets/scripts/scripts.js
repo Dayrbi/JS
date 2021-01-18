ba2419d5fae7babafe78710d407e0000877093b5
Vladimircss <ermolovich@gmail.com>
2021-01-18 20:24:43 +0300

    hw15

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,17 @@
+let h1 = document.getElementsByTagName("h1");
+console.log(h1);
+let ul = document.querySelector(".users-list");
+console.log(ul);
+let elements = document.querySelectorAll("ul > li");
+console.log(elements);
+elements.Style.marginTop = "50px";``
+// if(elements.style.color ="red"){
+//     setInterval(() => {
+//         elements.style.color = "blue"
+//     }, 2000);
+// }
+// else{
+// setInterval(() => {
+//     elements.style.color = "red"
+// }, 2000);
+// }
